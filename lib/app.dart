2f5924ade255e3ab5e34bdeaffa20afc4c3c5073
Tia <51142215+tiabenson@users.noreply.github.com>
2020-06-01 20:43:50 -0500

    hello world .dart files

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/helloScreen.dart';
+
+class App extends StatelessWidget
+{
+  @override
+
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(home: HelloScreen());  //contains MaterialApp widget
+  } //home: starter screen
+}
+
+   
